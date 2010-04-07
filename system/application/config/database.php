ca3be51ef4b68910fe98c3a7cba52392e4d933ca
Emmanuel <emmanuel@emmanuel-lap.(none)>
2010-04-07 18:04:08 -0500

    Credenciales para bdd modificadas

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -39,8 +39,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "291001";
-$db['default']['database'] = "capcie";
+$db['default']['password'] = "hola";
+$db['default']['database'] = "ge";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
